def752e61ac6ceb9ae471e97f9de2c554183978e
yongj.zhuang <yongj.zhuang@outlook.com>
2022-09-10 16:12:02 +0800

    TTime

--- web/dto/time.go
+++ web/dto/time.go
@@ -1,7 +1,7 @@
 package dto
 
 import (
-	"errors"
+	"strings"
 	"time"
 )
 
@@ -10,13 +10,30 @@ type WTime time.Time
 
 func (s WTime) MarshalJSON() ([]byte, error) {
 	t := time.Time(s)
-	if y := t.Year(); y < 0 || y >= 10000 {
-		return nil, errors.New("Time.MarshalJSON: year outside of range [0,9999]")
-	}
 
 	/*
 		Why they format time like this, it's frustrating :D
 		https://stackoverflow.com/questions/20234104/how-to-format-current-time-using-a-yyyymmddhhmmss-format
 	*/
 	return []byte(t.Format(`"02/01/2006 15:04"`)), nil
+}
+
+/* same as time.Time but will be serialized/deserialized using format '2006-01-02 15:04:05' */
+type TTime time.Time
+
+func (t TTime) MarshalJSON() ([]byte, error) {
+	tt := time.Time(t)
+
+	// yyyy/mm/dd hh:mm:ss
+	return []byte(tt.Format(`"2006-01-02 15:04:05"`)), nil
+}
+
+func (t *TTime) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	pt, err := time.Parse("2006-01-02 15:04:05", s)
+	if err != nil {
+		return err
+	}
+	*t = TTime(pt)
+	return nil
 }
