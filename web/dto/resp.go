455d38efbac98b309b31c55d058721367de02058
yongj.zhuang <yongj.zhuang@outlook.com>
2022-09-10 22:10:13 +0800

    Modified:  web/dto/resp.go

--- web/dto/resp.go
+++ web/dto/resp.go
@@ -10,7 +10,7 @@ import (
 type Resp struct {
 	ErrorCode string      `json:"errorCode"`
 	Msg       string      `json:"msg"`
-	Error     bool        `json:"hasError"`
+	Error     bool        `json:"error"`
 	Data      interface{} `json:"data"`
 }
 
