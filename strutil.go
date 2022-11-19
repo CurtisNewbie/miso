ba7a86b510e14c2d26204c58401151f5d0c0d422
yongj.zhuang <zhuangyongj@gmail.com>
2022-11-19 17:49:56 +0800

    More on strutil

--- strutil.go
+++ strutil.go
@@ -2,12 +2,37 @@ package gocommon
 
 import "strings"
 
+// Wrapper of Rune
+type RuneWrp struct {
+	c []rune
+}
+
+// Get length in terms of characters
+func (rw RuneWrp) Len() int {
+	return len(rw.c)
+}
+
+// Get string at index
+func (rw RuneWrp) StrAt(idx int) string {
+	return string(rw.c[idx])
+}
+
+// Get substring
+func (rw RuneWrp) Substr(start int, end int) string {
+	return string(rw.c[start:end])
+}
+
 // Check if the string is empty
 func IsEmpty(s *string) bool {
 	if s == nil || strings.TrimSpace(*s) == "" {
 		return true
 	}
 	return false
+}
+
+// Get RuneWrp from string
+func GetRuneWRp(s string) RuneWrp {
+	return RuneWrp{c: []rune(s)}
 }
 
 // Check if the string is empty
@@ -34,4 +59,17 @@ func Substr(s string, start int, end int) string {
 // Return len of string (rune)
 func StrLen(s string) int {
 	return len([]rune(s))
+}
+
+// Get last char
+func LastChar(s string) (length int, lastChar string) {
+	rs := []rune(s)
+	l := len(rs)
+
+	// empty string
+	if l < 1 {
+		return l, ""
+	}
+
+	return l, string(rs[l-1])
 }
