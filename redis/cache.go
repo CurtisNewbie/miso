6cdbd7e32f111ea837b74d598b8f46ba2f40664c
yongj.zhuang <zhuangyongj@gmail.com>
2023-01-19 15:05:16 +0800

    Impl lazy version of RCache

--- redis/cache.go
+++ redis/cache.go
@@ -1,16 +1,55 @@
 package redis
 
 import (
+	"sync"
 	"time"
 
 	"github.com/bsm/redislock"
 	"github.com/go-redis/redis"
 )
 
+// RCache
 type RCache struct {
 	rclient *redis.Client
 	rlocker *redislock.Client
 	exp     time.Duration
+}
+
+// Lazy version of RCache
+type LazyRCache struct {
+	rcacheSupplier func() RCache
+	_rcache        *RCache
+	rwmu           sync.RWMutex
+}
+
+func (r *LazyRCache) rcache() *RCache {
+	r.rwmu.RLock()
+	if r._rcache != nil {
+		defer r.rwmu.RUnlock()
+		return r._rcache
+	}
+	r.rwmu.RUnlock()
+
+	r.rwmu.Lock()
+	defer r.rwmu.Unlock()
+	c := r.rcacheSupplier()
+	r._rcache = &c
+	return r._rcache
+}
+
+// Put value to cache
+func (r *LazyRCache) Put(key string, val string) error {
+	return r.rcache().Put(key, val)
+}
+
+// Get from cache
+func (r *LazyRCache) Get(key string) (val string, e error) {
+	return r.rcache().Get(key)
+}
+
+// Get from cache else run supplier
+func (r *LazyRCache) GetElse(key string, supplier func() string) (val string, e error) {
+	return r.rcache().GetElse(key, supplier)
 }
 
 // Put value to cache
@@ -72,4 +111,9 @@ func (r *RCache) GetElse(key string, supplier func() string) (val string, e erro
 // Create new RCache
 func NewRCache(exp time.Duration) RCache {
 	return RCache{rclient: GetRedis(), rlocker: ObtainRLocker(), exp: exp}
+}
+
+// Create new lazy RCache
+func NewLazyRCache(exp time.Duration) LazyRCache {
+	return LazyRCache{rcacheSupplier: func() RCache { return NewRCache(exp) }}
 }
--- redis/cache_test.go
+++ redis/cache_test.go
@@ -28,3 +28,26 @@ func TestRCache(t *testing.T) {
 		t.Fatalf("val '%v' != \"2\"", val)
 	}
 }
+
+func TestLKazyRCache(t *testing.T) {
+	common.LoadConfigFromFile("../app-conf-dev.yml")
+	InitRedisFromProp()
+
+	keypre := "test:rcache:key:"
+	exp := 60 * time.Second
+
+	rcache := NewLazyRCache(exp)
+	e := rcache.Put(keypre + "1", "2")
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	val, e := rcache.GetElse(keypre + "1", nil)
+	if e != nil {
+		t.Fatal(e)
+	}
+	if val != "2" {
+		t.Fatalf("val '%v' != \"2\"", val)
+	}
+}
+
