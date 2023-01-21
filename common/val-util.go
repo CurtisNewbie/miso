356af4ba291c76c2842e97859f5dde751780fb6c
yongj.zhuang <zhuangyongj@gmail.com>
2023-01-21 14:36:32 +0800

    Optional

--- common/val-util.go
+++ common/val-util.go
@@ -7,6 +7,22 @@ const (
 	BOOL_STR_FALSE = "false"
 )
 
+// Optional value, useful for passing zero value struct
+type Optional[T any] struct {
+	Val       T
+	IsPresent bool
+}
+
+// Empty Optional
+func EmptyOpt[T any]() Optional[T] {
+	return Optional[T]{IsPresent: false}
+}
+
+// Optional with value present
+func OptWith[T any](t T) Optional[T] {
+	return Optional[T]{Val: t, IsPresent: true}
+}
+
 func IsTrue(boolStr string) bool {
 	return strings.ToLower(boolStr) == "true"
 }
