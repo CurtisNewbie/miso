8d4a89ce66cc28e6da725e554c7be27ec83889c9
yongj.zhuang <yongj.zhuang@outlook.com>
2023-01-28 23:39:54 +0800

    Remove use of ptr

--- common/strutil_test.go
+++ common/strutil_test.go
@@ -7,47 +7,45 @@ import (
 func TestFirstChar(t *testing.T) {
 	var l int
 	var c string
-	var cp *string = &c
-	if l, *cp = FirstChar(""); l != 0 || *cp != "" {
+	if l, c = FirstChar(""); l != 0 || c != "" {
 		t.Error()
 		return
 	}
-	t.Logf("l: %d, c: '%s'", l, *cp)
+	t.Logf("l: %d, c: '%s'", l, c)
 
-	if l, *cp = FirstChar("A"); l != 1 || *cp != "A" {
+	if l, c = FirstChar("A"); l != 1 || c != "A" {
 		t.Error()
 		return
 	}
-	t.Logf("l: %d, c: '%s'", l, *cp)
+	t.Logf("l: %d, c: '%s'", l, c)
 
-	if l, *cp = FirstChar("abccc d"); l != 7 || *cp != "a" {
+	if l, c = FirstChar("abccc d"); l != 7 || c != "a" {
 		t.Error()
 		return
 	}
-	t.Logf("l: %d, c: '%s'", l, *cp)
+	t.Logf("l: %d, c: '%s'", l, c)
 }
 
 func TestLastChar(t *testing.T) {
 	var l int
 	var c string
-	var cp *string = &c
-	if l, *cp = LastChar(""); l != 0 || *cp != "" {
+	if l, c = LastChar(""); l != 0 || c != "" {
 		t.Error()
 		return
 	}
-	t.Logf("l: %d, c: '%s'", l, *cp)
+	t.Logf("l: %d, c: '%s'", l, c)
 
-	if l, *cp = LastChar("A"); l != 1 || *cp != "A" {
+	if l, c = LastChar("A"); l != 1 || c != "A" {
 		t.Error()
 		return
 	}
-	t.Logf("l: %d, c: '%s'", l, *cp)
+	t.Logf("l: %d, c: '%s'", l, c)
 
-	if l, *cp = LastChar("abccc d"); l != 7 || *cp != "d" {
+	if l, c = LastChar("abccc d"); l != 7 || c != "d" {
 		t.Error()
 		return
 	}
-	t.Logf("l: %d, c: '%s'", l, *cp)
+	t.Logf("l: %d, c: '%s'", l, c)
 }
 
 func TestRuneWrp(t *testing.T) {
