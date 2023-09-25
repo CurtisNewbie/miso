994d5e05a6429f9e6a782944a2496c1c15946c9a
yongj.zhuang <yongj.zhuang@outlook.com>
2023-09-25 15:46:58 +0800

    Implements RLock

--- miso/rlock.go
+++ miso/rlock.go
@@ -10,6 +10,10 @@ import (
 type Runnable func() error
 type LRunnable[T any] func() (T, error)
 
+const (
+	defaultBackoffSteps = 200 // default backoff steps, 200 (5ms) = 1s
+)
+
 var (
 	lock_lease_time = time.Duration(30_000) * time.Millisecond
 	refresh_time    = time.Duration(10_000) * time.Millisecond
@@ -34,7 +38,7 @@ May return 'redislock:.ErrNotObtained' when it fails to obtain the lock.
 */
 func RLockRun[T any](ec Rail, key string, runnable LRunnable[T]) (T, error) {
 	var t T
-	locker := ObtainRLocker()
+	var locker *redislock.Client = ObtainRLocker()
 	lock, err := locker.Obtain(key, lock_lease_time, &redislock.Options{
 		RetryStrategy: redislock.LimitRetry(redislock.LinearBackoff(10*time.Millisecond), 100),
 	})
@@ -82,11 +86,140 @@ Lock and run the runnable using Redis
 
 The maximum time wait for the lock is 1s, retry every 10ms.
 
-May return 'redislock:.ErrNotObtained' when it fails to obtain the lock.
+May return 'redislock.ErrNotObtained' when it fails to obtain the lock.
 */
 func RLockExec(ec Rail, key string, runnable Runnable) error {
 	_, e := RLockRun(ec, key, func() (any, error) {
 		return nil, runnable()
 	})
 	return e
+}
+
+// RLock
+type RLock struct {
+	rail            Rail
+	key             string
+	cancelRefresher func()
+	_isLocked       int32
+	lock            *redislock.Lock
+	backoffWindow   time.Duration
+	backoffSteps    int
+}
+
+// RLock Configuration.
+type RLockConfig struct {
+	// Backoff duration.
+	//
+	// This is not an exact configuration. Linear back off strategy is used with a window size of 5ms, which means RLock will attempt to acquire the lock every 5ms.
+	//
+	// The number of times we may attempt to acquire the lock is called steps, which is by default 200 (that's 1s = 200 * 5ms).
+	// When BackoffDuration is provided, this duration is divided by 5ms to convert to steps and then used by RLock.
+	BackoffDuration time.Duration
+}
+
+// Create new RLock with default backoff configuration (5ms backoff window, 200 attempts, i.e., retry for 1s).
+func NewRLock(rail Rail, key string) RLock {
+	return NewCustomRLock(rail, key, RLockConfig{})
+}
+
+// Create customized RLock.
+func NewCustomRLock(rail Rail, key string, config RLockConfig) RLock {
+	r := RLock{
+		rail:            rail,
+		key:             key,
+		_isLocked:       0,
+		cancelRefresher: nil,
+		lock:            nil,
+		backoffWindow:   5 * time.Millisecond,
+		backoffSteps:    defaultBackoffSteps,
+	}
+
+	if config.BackoffDuration > r.backoffWindow {
+		// this is merely an approximate
+		nsteps := int64(config.BackoffDuration) / int64(r.backoffWindow)
+		r.backoffSteps = int(nsteps)
+		rail.Debugf("Update backoff steps to %v", nsteps)
+	}
+
+	rail.Debugf("Created RLock for key: '%v', with backoffWindow: %v, backoffSteps: %v", r.key, r.backoffWindow, r.backoffSteps)
+	return r
+}
+
+func (r *RLock) isLocked() bool {
+	if r.lock == nil {
+		return false
+	}
+	return atomic.LoadInt32(&r._isLocked) == 1
+}
+
+func (r *RLock) setLocked() {
+	atomic.StoreInt32(&r._isLocked, 1)
+}
+
+func (r *RLock) setUnlocked() {
+	atomic.StoreInt32(&r._isLocked, 0)
+}
+
+// Acquire lock.
+func (r *RLock) Lock() error {
+	rlocker := ObtainRLocker()
+	lock, err := rlocker.Obtain(r.key, lock_lease_time, &redislock.Options{
+		RetryStrategy: redislock.LimitRetry(redislock.LinearBackoff(r.backoffWindow), r.backoffSteps),
+	})
+
+	if err != nil {
+		return TraceErrf(err, "failed to obtain lock, key: %v", r.key)
+	}
+	r.rail.Debugf("Obtained lock for key '%s'", r.key)
+
+	r.setLocked()
+	r.lock = lock
+
+	subrail, cancel := r.rail.WithCancel()
+	r.cancelRefresher = cancel
+
+	go func(subrail Rail) {
+		ticker := time.NewTicker(refresh_time)
+		for {
+			select {
+			case <-ticker.C:
+				if err := lock.Refresh(lock_lease_time, nil); err != nil {
+					subrail.Warnf("Failed to refresh RLock for '%v'", r.key)
+				} else {
+					subrail.Debugf("Refreshed rlock for '%v'", r.key)
+				}
+			case <-subrail.Ctx.Done():
+				subrail.Debugf("RLock Refresher cancelled for '%v'", r.key)
+				return
+			}
+		}
+	}(subrail)
+
+	return nil
+}
+
+// Attempt to Unlock.
+//
+// If the lock is not obtained, method call will be ignored.
+func (r *RLock) Unlock() error {
+	if !r.isLocked() {
+		return nil
+	}
+
+	if r.cancelRefresher != nil {
+		r.cancelRefresher()
+	}
+
+	if r.lock != nil {
+		err := r.lock.Release()
+		if err != nil {
+			r.rail.Errorf("Failed to release lock for key '%s', err: %v", r.key, err)
+			return err
+		} else {
+			r.rail.Debugf("Released lock for key '%s'", r.key)
+		}
+	}
+	r.setUnlocked()
+	r.lock = nil
+	return nil
 }
--- miso/rlock_test.go
+++ miso/rlock_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func TestRLock(t *testing.T) {
+func TestRLockCallbacks(t *testing.T) {
 	logrus.SetLevel(logrus.DebugLevel)
 
 	c := EmptyRail()
@@ -50,5 +50,95 @@ func TestRLock(t *testing.T) {
 	if atomic.LoadInt32(&violated) == 1 {
 		t.Fatal()
 	}
+}
 
+func TestRLock(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+
+	rail := EmptyRail()
+	LoadConfigFromFile("../app-conf-dev.yml", rail)
+	if _, e := InitRedisFromProp(rail); e != nil {
+		t.Fatal(e)
+	}
+	rail.SetLogLevel("debug")
+
+	var violated int32 = 0
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		wg.Wait()
+
+		lock := NewRLock(rail, "test:rlock")
+		rail.Infof("routine - routine attempts to get lock")
+		start := time.Now()
+
+		err := lock.Lock()
+		if err == nil {
+			rail.Infof("routine - test failed, condition violated, err == nil")
+			atomic.StoreInt32(&violated, 1)
+		} else {
+			rail.Infof("routine - test passed, timed-out, %v, attempted for %v", err, time.Since(start))
+		}
+	}()
+
+	lock := NewRLock(rail, "test:rlock")
+	err := lock.Lock()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rail.Infof("main - inside lock")
+	wg.Done()
+	time.Sleep(3 * time.Second)
+
+	if err := lock.Unlock(); err != nil {
+		t.Fatal(err)
+	}
+
+	if atomic.LoadInt32(&violated) == 1 {
+		t.Fatal()
+	}
+}
+
+func TestRLockCount(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+
+	rail := EmptyRail()
+	LoadConfigFromFile("../app-conf-dev.yml", rail)
+	if _, e := InitRedisFromProp(rail); e != nil {
+		t.Fatal(e)
+	}
+	rail.SetLogLevel("debug")
+
+	total := 1000
+	var count int32 = 0
+
+	var wg sync.WaitGroup
+	for i := 0; i < total; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			lock := NewCustomRLock(rail, "test:rlock", RLockConfig{
+				BackoffDuration: time.Second * 2,
+			})
+
+			err := lock.Lock()
+			if err != nil {
+				t.Logf("failed to obtain lock, %v", err)
+				return
+			}
+			defer lock.Unlock()
+
+			atomic.StoreInt32(&count, atomic.LoadInt32(&count)+1)
+		}()
+	}
+	wg.Wait()
+
+	actual := atomic.LoadInt32(&count)
+	if actual != 1000 {
+		t.Fatalf("incorrect count, actual: %v", actual)
+	}
 }
