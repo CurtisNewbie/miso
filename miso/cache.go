e3903b497fb6390aa91888c1594303eba1f04eab
yongj.zhuang <yongj.zhuang@outlook.com>
2023-09-27 18:26:10 +0800

    Avoid too frequent cleanup when the cache is full

--- miso/cache.go
+++ miso/cache.go
@@ -48,13 +48,15 @@ func newTBucket[T any](val T) tbucket[T] {
 
 // Simple concurrent safe, in-memory lru cache implementation.
 type ttlCache[T any] struct {
-	cache   map[string]tbucket[T]
-	mu      sync.RWMutex
-	ttl     time.Duration
-	maxSize int
+	cache       map[string]tbucket[T]
+	mu          sync.RWMutex
+	ttl         time.Duration
+	maxSize     int
+	lastCleanup time.Time
 }
 
 func (tc *ttlCache[T]) cleanup(now time.Time, partitions int) {
+	tc.lastCleanup = time.Now()
 
 	// we divide the whole cache into N paritiions, we only do partial cleanup for the first persudo partition
 	clen := len(tc.cache)
@@ -105,18 +107,22 @@ func (tc *ttlCache[T]) Get(key string, elseGet func() (T, bool)) (T, bool) {
 	if ok {
 
 		maxSizeExceeded := func() bool { return tc.maxSize > 0 && len(tc.cache) > tc.maxSize }
-
-		// if we have already exceeded the max size, we attempt to do some cleanup
-		if maxSizeExceeded() {
-			tc.cleanup(now, 10)
-		}
-
-		// after the cleanup, the max size is still exceeded, to avoid blowing up the cache, we return the value directly
-		if maxSizeExceeded() {
+		if !maxSizeExceeded() {
+			tc.cache[key] = newTBucket(v)
 			return v, true
 		}
 
-		tc.cache[key] = newTBucket(v)
+		// if we have already exceeded the max size, we attempt to do some cleanup
+		// if the cache is already full, and we cannot spare any extra space at all, we have to avoid doing cleanup all the time
+		// 10s cleanup gap is merely a guess.
+		if tc.lastCleanup.Before(now.Add(-10 * time.Second)) {
+			tc.cleanup(now, 10)
+
+			// after the cleanup, the max size may still be exceeded, we must avoid blowing up the cache
+			if !maxSizeExceeded() {
+				tc.cache[key] = newTBucket(v)
+			}
+		}
 		return v, true
 	}
 
@@ -150,8 +156,9 @@ func NewTTLCache[T any](ttl time.Duration, maxSize int) TTLCache[T] {
 		maxSize = 0
 	}
 	return &ttlCache[T]{
-		cache:   map[string]tbucket[T]{},
-		ttl:     ttl,
-		maxSize: maxSize,
+		cache:       map[string]tbucket[T]{},
+		ttl:         ttl,
+		maxSize:     maxSize,
+		lastCleanup: time.Now(),
 	}
 }
