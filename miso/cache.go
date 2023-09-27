aa97d9cbe9732834a185c0b8c50ecd0173dd56fe
yongj.zhuang <yongj.zhuang@outlook.com>
2023-09-27 18:10:13 +0800

    TTLCache maintains maxSize to avoid blowing it up

--- miso/cache.go
+++ miso/cache.go
@@ -48,27 +48,74 @@ func newTBucket[T any](val T) tbucket[T] {
 
 // Simple concurrent safe, in-memory lru cache implementation.
 type ttlCache[T any] struct {
-	cache map[string]tbucket[T]
-	mu    sync.Mutex
-	ttl   time.Duration
+	cache   map[string]tbucket[T]
+	mu      sync.RWMutex
+	ttl     time.Duration
+	maxSize int
+}
+
+func (tc *ttlCache[T]) cleanup(now time.Time, partitions int) {
+
+	// we divide the whole cache into N paritiions, we only do partial cleanup for the first persudo partition
+	clen := len(tc.cache)
+	partition_size := clen
+	if clen > partitions {
+		partition_size = clen / partitions
+	}
+	i := 0
+
+	// iterate the cache to cleanup dead buckets, the ordering of keys accessed is not deterministic
+	for k := range tc.cache {
+		if i > partition_size {
+			return
+		}
+		i += 1
+		if buk := tc.cache[k]; !buk.alive(now, tc.ttl) {
+			delete(tc.cache, k)
+		}
+	}
 }
 
 func (tc *ttlCache[T]) Get(key string, elseGet func() (T, bool)) (T, bool) {
-	tc.mu.Lock()
-	defer tc.mu.Unlock()
-
 	now := time.Now()
 	var v T
 
+	// obtain read lock, check if the key exists and is alive
+	// only obtain write lock when we need to load the key
+	tc.mu.RLock()
 	buk, ok := tc.cache[key]
+	if ok && buk.alive(now, tc.ttl) {
+		defer tc.mu.RUnlock()
+		return buk.val, true
+	}
+	tc.mu.RUnlock()
+
+	// obtain write lock
+	tc.mu.Lock()
+	defer tc.mu.Unlock()
+
+	// check again, race condition is possible
+	buk, ok = tc.cache[key]
 	if ok && buk.alive(now, tc.ttl) {
 		return buk.val, true
 	}
-
 	evictable := ok // if ok, then v must be evictable
 
 	v, ok = elseGet()
 	if ok {
+
+		maxSizeExceeded := func() bool { return tc.maxSize > 0 && len(tc.cache) > tc.maxSize }
+
+		// if we have already exceeded the max size, we attempt to do some cleanup
+		if maxSizeExceeded() {
+			tc.cleanup(now, 10)
+		}
+
+		// after the cleanup, the max size is still exceeded, to avoid blowing up the cache, we return the value directly
+		if maxSizeExceeded() {
+			return v, true
+		}
+
 		tc.cache[key] = newTBucket(v)
 		return v, true
 	}
@@ -92,13 +139,19 @@ func (tc *ttlCache[T]) Put(key string, t T) {
 // Each k/v is associated with a timestamp. Each time a key lookup occurs, it checks whether the k/v is still valid by comparing the timestamp with time.Now().
 //
 // If the k/v is no longer 'alive', or the cache doesn't have the key, supplier func for the value is called, and the returned value is then cached.
-//
 // I.e., each k/v is evicted only at key lookup, there is no secret go-routine running to do the clean-up, the overhead for maintaining the cache is relatively small.
 //
+// For the max size, TTLCache will try it's best to maintain it, but it's quite possible that all values in the cache are 'alive'. Whenever the max size is violated,
+// TTLCache will first do a partial cleanup, if the max size is still violated after the cleanup, the value is returned directly without going into the cache.
+//
 // The returned TTLCache can be used concurrently.
-func NewTTLCache[T any](ttl time.Duration) TTLCache[T] {
+func NewTTLCache[T any](ttl time.Duration, maxSize int) TTLCache[T] {
+	if maxSize < 0 {
+		maxSize = 0
+	}
 	return &ttlCache[T]{
-		cache: map[string]tbucket[T]{},
-		ttl:   ttl,
+		cache:   map[string]tbucket[T]{},
+		ttl:     ttl,
+		maxSize: maxSize,
 	}
 }
--- miso/cache_test.go
+++ miso/cache_test.go
@@ -1,6 +1,8 @@
 package miso
 
 import (
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -12,7 +14,7 @@ func TestTTLCacheNormal(t *testing.T) {
 		name string
 	}
 
-	cache := NewTTLCache[ttlDummy](1 * time.Minute)
+	cache := NewTTLCache[ttlDummy](1*time.Minute, 5)
 
 	cnt := 0
 	elseGet := func() (ttlDummy, bool) {
@@ -54,7 +56,7 @@ func TestTTLCacheEvicted(t *testing.T) {
 		name string
 	}
 
-	cache := NewTTLCache[ttlDummy](1 * time.Second)
+	cache := NewTTLCache[ttlDummy](1*time.Second, 5)
 
 	cnt := 0
 	elseGet := func() (ttlDummy, bool) {
@@ -88,5 +90,107 @@ func TestTTLCacheEvicted(t *testing.T) {
 
 	if cnt != 2 {
 		t.Fatalf("cnt should be 2, but %v", cnt)
+	}
+}
+
+func TestTTLCacheMaxSize(t *testing.T) {
+	rail := EmptyRail()
+
+	type ttlDummy struct {
+		name string
+	}
+
+	cache := NewTTLCache[ttlDummy](1*time.Second, 10)
+
+	cnt := 0
+	elseGet := func() (ttlDummy, bool) {
+		cnt += 1
+		rail.Infof("elseGet %v", cnt)
+		return ttlDummy{
+			name: "myDummy",
+		}, true
+	}
+
+	for i := 0; i < 10; i++ {
+		cache.Get(ERand(5), elseGet)
+	}
+
+	if cnt != 10 {
+		t.Fatalf("cnt should be 10, but %v", cnt)
+	}
+
+	// all the key should be evicted already
+	time.Sleep(2 * time.Second)
+
+	v, ok := cache.Get("abc", elseGet)
+	if !ok {
+		t.Fatal("not ok")
+	}
+
+	if v.name != "myDummy" {
+		t.Fatalf("name not myDummy, but %v", v.name)
+	}
+
+	if cnt != 11 {
+		t.Fatalf("cnt should be 11, but %v", cnt)
+	}
+
+	v, ok = cache.Get("abc", elseGet)
+	if !ok {
+		t.Fatal("not ok")
+	}
+
+	if v.name != "myDummy" {
+		t.Fatalf("name not myDummy, but %v", v.name)
+	}
+
+	if cnt != 11 {
+		t.Fatalf("cnt should be 11, but %v", cnt)
+	}
+}
+
+func TestTTLCacheConcurrent(t *testing.T) {
+	rail := EmptyRail()
+
+	type ttlDummy struct {
+		name string
+	}
+
+	cache := NewTTLCache[ttlDummy](1*time.Minute, 10)
+
+	var cnt int32 = 0
+	elseGet := func() (ttlDummy, bool) {
+		rail.Infof("elseGet %v", atomic.AddInt32(&cnt, 1))
+		return ttlDummy{
+			name: "myDummy",
+		}, true
+	}
+
+	var failed int32 = 0
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(t *testing.T) {
+			defer wg.Done()
+			v, ok := cache.Get("abc", elseGet)
+			if !ok {
+				t.Log("not ok")
+				atomic.AddInt32(&failed, 1)
+			}
+
+			if v.name != "myDummy" {
+				t.Logf("name not myDummy, but %v", v.name)
+				atomic.AddInt32(&failed, 1)
+			}
+		}(t)
+	}
+	wg.Wait()
+
+	if cnt != 1 {
+		t.Fatalf("cnt should be 10, but %v", cnt)
+	}
+	if failed > 0 {
+		t.Fail()
 	}
 }
