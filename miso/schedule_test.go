440b35790fca62878b5f6047a68b1d0c8dfb9268
yongj.zhuang <zhuangyongj@gmail.com>
2023-11-14 00:05:57 +0800

    Job and tasks must be named, propagate Rail into scheduler.go

--- miso/schedule_test.go
+++ miso/schedule_test.go
@@ -11,19 +11,21 @@ func TestScheduleCron(t *testing.T) {
 	var noc int32 = 0
 
 	t.Log("Yo")
-	err := ScheduleCron("*/1 * * * * *", true, func() {
+	err := ScheduleCron("myjob", "*/1 * * * * *", true, func(rail Rail) error {
 		time.Sleep(1 * time.Second)
 		atomic.AddInt32(&yoc, 1)
 		t.Log("Yo")
+		return nil
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = ScheduleCron("*/1 * * * * *", true, func() {
+	err = ScheduleCron("myjob", "*/1 * * * * *", true, func(rail Rail) error {
 		time.Sleep(1 * time.Second)
 		atomic.AddInt32(&noc, 1)
 		t.Log("No")
+		return nil
 	})
 	if err != nil {
 		t.Fatal(err)
