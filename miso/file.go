44ed6636da31ff1d8405ee224f2f1beec62c52ec
yongj.zhuang <yongj.zhuang@outlook.com>
2024-01-02 17:14:31 +0800

    Convenient funcs for opening files

--- miso/file.go
+++ miso/file.go
@@ -15,3 +15,18 @@ func FileExists(path string) (bool, error) {
 
 	return false, e
 }
+
+// Open file with 0666 permission.
+func OpenFile(name string, flag int) (*os.File, error) {
+	return os.OpenFile(name, flag, 0666)
+}
+
+// Create writable file with 0666 permisson.
+func WritableFile(name string) (*os.File, error) {
+	return OpenFile(name, os.O_CREATE|os.O_WRONLY)
+}
+
+// Create appendable file with 0666 permisson.
+func AppendableFile(name string) (*os.File, error) {
+	return OpenFile(name, os.O_CREATE|os.O_APPEND)
+}
