9909a1b8fef02783f7740f8e96102c852021fe91
yongj.zhuang <yongj.zhuang@outlook.com>
2024-01-13 14:32:25 +0800

    Impl ReadWriteFile()

--- miso/file.go
+++ miso/file.go
@@ -2,6 +2,11 @@ package miso
 
 import "os"
 
+const (
+	// Default File Mode
+	DefFileMode = 0666
+)
+
 // Check if file exists
 func FileExists(path string) (bool, error) {
 	_, e := os.Stat(path)
@@ -18,15 +23,20 @@ func FileExists(path string) (bool, error) {
 
 // Open file with 0666 permission.
 func OpenFile(name string, flag int) (*os.File, error) {
-	return os.OpenFile(name, flag, 0666)
+	return os.OpenFile(name, flag, DefFileMode)
 }
 
-// Create writable file with 0666 permisson.
+// Create writable file with 0666 permission.
 func WritableFile(name string) (*os.File, error) {
 	return OpenFile(name, os.O_CREATE|os.O_WRONLY)
 }
 
-// Create appendable file with 0666 permisson.
+// Create appendable file with 0666 permission.
 func AppendableFile(name string) (*os.File, error) {
 	return OpenFile(name, os.O_CREATE|os.O_APPEND)
+}
+
+// Create readable & writable file with 0666 permission.
+func ReadWriteFile(name string) (*os.File, error) {
+	return OpenFile(name, os.O_CREATE|os.O_RDWR)
 }
