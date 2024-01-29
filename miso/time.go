e8f4741b66229775e8b0ccbc4a97fde490924075
yongj.zhuang <yongj.zhuang@outlook.com>
2024-01-29 09:52:35 +0800

    Reorganize pointer/value receiver for time.go

--- miso/time.go
+++ miso/time.go
@@ -30,6 +30,36 @@ func (t ETime) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf("%d", tt.UnixMilli())), nil
 }
 
+func (t ETime) String() string {
+	return time.Time(t).String()
+}
+
+func (t ETime) ToTime() time.Time {
+	return time.Time(t)
+}
+
+func (t ETime) UnixMilli() int64 {
+	return t.ToTime().UnixMilli()
+}
+
+func (t ETime) FormatClassic() string {
+	return t.ToTime().Format("2006/01/02 15:04:05")
+}
+
+func (t ETime) FormatClassicLocale() string {
+	return t.ToTime().Format("2006/01/02 15:04:05 (MST)")
+}
+
+// Implements driver.Valuer in database/sql.
+func (et ETime) Value() (driver.Value, error) {
+	t := time.Time(et)
+	if t.IsZero() {
+		return nil, nil
+	}
+	return t.Format(sqlTimeFormat), nil
+}
+
+// implements decorder.Unmarshaler in encoding/json.
 func (t *ETime) UnmarshalJSON(b []byte) error {
 	millisec, err := strconv.ParseInt(string(b), 10, 64)
 	if err != nil {
@@ -41,34 +71,24 @@ func (t *ETime) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-func (t ETime) String() string {
-	return time.Time(t).String()
-}
-
-func (t *ETime) ToTime() time.Time {
-	return time.Time(*t)
-}
-
-func (t *ETime) UnixMilli() int64 {
-	return t.ToTime().UnixMilli()
-}
-
-func (t *ETime) FormatClassic() string {
-	return t.ToTime().Format("2006/01/02 15:04:05")
-}
-
-func (t *ETime) FormatClassicLocale() string {
-	return t.ToTime().Format("2006/01/02 15:04:05 (MST)")
-}
-
-// database driver -> ETime
-func (et *ETime) Scan(value interface{}) (err error) {
+// Implements sql.Scanner in database/sql.
+func (et *ETime) Scan(value interface{}) error {
 	switch v := value.(type) {
 	case time.Time:
 		*et = ETime(v)
 	case []byte:
 		var t time.Time
-		t, err = time.Parse(sqlTimeFormat, string(v))
+		t, err := time.Parse(sqlTimeFormat, string(v))
+		if err != nil {
+			return err
+		}
+		*et = ETime(t)
+	case string:
+		var t time.Time
+		t, err := time.Parse(sqlTimeFormat, v)
+		if err != nil {
+			return err
+		}
 		*et = ETime(t)
 	case int64, int, uint, uint64, int32, uint32, int16, uint16, *int64, *int, *uint, *uint64, *int32, *uint32, *int16, *uint16:
 		val := reflect.Indirect(reflect.ValueOf(v)).Int()
@@ -78,16 +98,8 @@ func (et *ETime) Scan(value interface{}) (err error) {
 			*et = ETime(time.Unix(val, 0)) // in sec
 		}
 	default:
-		err = fmt.Errorf("invalid field type '%v' for ETime, unable to convert, %#v", reflect.TypeOf(value), v)
+		err := fmt.Errorf("invalid field type '%v' for ETime, unable to convert, %#v", reflect.TypeOf(value), v)
+		return err
 	}
-	return
-}
-
-// ETime -> database driver
-func (et ETime) Value() (driver.Value, error) {
-	t := time.Time(et)
-	if t.IsZero() {
-		return nil, nil
-	}
-	return t.Format(sqlTimeFormat), nil
+	return nil
 }
