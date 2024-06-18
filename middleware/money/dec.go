22c2cf6b997e80599719e93222b39cac1b090a3a
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-18 17:41:02 +0800

    Add FormatAmt

--- middleware/money/dec.go
+++ middleware/money/dec.go
@@ -5,13 +5,22 @@ import (
 )
 
 // Create new signed arbitrary-precision decimal with appropriate scale for the currency and HalfEven rounding mode.
-func NewInfDec(s string, currency string) (*inf.Dec, error) {
+func NewInfDec(amt string, currency string) (*inf.Dec, error) {
 	sc, err := Scale(currency)
 	if err != nil {
 		return nil, err
 	}
 	d := new(inf.Dec)
-	d.SetString(s)
+	d.SetString(amt)
 	d.Round(d, inf.Scale(sc), inf.RoundHalfEven)
 	return d, nil
+}
+
+// Format amount to appropriate rounding and scale for the currency.
+func FormatAmt(amt string, currency string) string {
+	dec, err := NewInfDec(amt, currency)
+	if err != nil {
+		return amt
+	}
+	return dec.String()
 }
