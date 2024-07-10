d106fe95b245397db67b51caf83c0f94a6abaf13
yongj.zhuang <yongj.zhuang@outlook.com>
2024-07-10 10:39:45 +0800

    Polishing

--- middleware/mysql/page.go
+++ middleware/mysql/page.go
@@ -6,63 +6,17 @@ import (
 	"gorm.io/gorm"
 )
 
-const (
-	DefaultPageLimit = 30
-)
-
-type Paging struct {
-	Limit int `json:"limit" desc:"page limit"`
-	Page  int `json:"page" desc:"page number, 1-based"`
-	Total int `json:"total" desc:"total count"`
-}
-
-type PageRes[T any] struct {
-	Page    Paging `json:"paging" desc:"pagination parameters"`
-	Payload []T    `json:"payload" desc:"payload values in current page"`
-}
-
-func (p Paging) GetPage() int {
-	if p.Page < 1 {
-		return 1
-	}
-	return p.Page
-}
-
-func (p Paging) GetOffset() int {
-	return (p.GetPage() - 1) * p.GetLimit()
-}
-
-func (p Paging) GetLimit() int {
-	if p.Limit < 1 {
-		p.Limit = DefaultPageLimit
-	}
-	return p.Limit
-}
-
-func (p Paging) ToRespPage(total int) Paging {
-	return RespPage(p, total)
-}
-
-/* Build Paging for response */
-func RespPage(reqPage Paging, total int) Paging {
-	return Paging{
-		Limit: reqPage.GetLimit(),
-		Page:  reqPage.GetPage(),
-		Total: total,
-	}
-}
-
 type PageQueryBuilder func(tx *gorm.DB) *gorm.DB
 
 // Create param for page query.
 type QueryPageParam[V any] struct {
-	reqPage     Paging            // Request Paging Param.
+	reqPage     miso.Paging       // Request Paging Param.
 	selectQuery PageQueryBuilder  // Add SELECT query and ORDER BY query, e.g., return tx.Select(`*`).
 	baseQuery   PageQueryBuilder  // Base query, e.g., return tx.Table(`myTable`).Where(...)
 	forEach     util.Transform[V] // callback triggered on each record, the value returned will overwrite the value passed in.
 }
 
-func (q *QueryPageParam[V]) WithPage(p Paging) *QueryPageParam[V] {
+func (q *QueryPageParam[V]) WithPage(p miso.Paging) *QueryPageParam[V] {
 	q.reqPage = p
 	return q
 }
@@ -83,7 +37,7 @@ func (q *QueryPageParam[V]) ForEach(t util.Transform[V]) *QueryPageParam[V] {
 }
 
 // Execute paging query
-func (q *QueryPageParam[V]) Exec(rail miso.Rail, tx *gorm.DB) (PageRes[V], error) {
+func (q *QueryPageParam[V]) Exec(rail miso.Rail, tx *gorm.DB) (miso.PageRes[V], error) {
 	return QueryPage(rail, tx, *q)
 }
 
@@ -91,7 +45,7 @@ func (q *QueryPageParam[V]) Exec(rail miso.Rail, tx *gorm.DB) (PageRes[V], error
 //
 // COUNT query is called first, if none is found (i.e., COUNT(...) == 0), this method
 // will not call the actual SELECT query to avoid unnecessary performance lost.
-func QueryPage[Res any](rail miso.Rail, tx *gorm.DB, p QueryPageParam[Res]) (PageRes[Res], error) {
+func QueryPage[Res any](rail miso.Rail, tx *gorm.DB, p QueryPageParam[Res]) (miso.PageRes[Res], error) {
 	newQuery := func() *gorm.DB {
 		return p.baseQuery(tx)
 	}
@@ -122,7 +76,7 @@ func QueryPage[Res any](rail miso.Rail, tx *gorm.DB, p QueryPageParam[Res]) (Pag
 		return payload, nil
 	})
 
-	var res PageRes[Res]
+	var res miso.PageRes[Res]
 	total, err := countFuture.Get()
 	if err != nil {
 		return res, err
@@ -133,7 +87,7 @@ func QueryPage[Res any](rail miso.Rail, tx *gorm.DB, p QueryPageParam[Res]) (Pag
 		return res, err
 	}
 
-	res = PageRes[Res]{Payload: payload, Page: RespPage(p.reqPage, total)}
+	res = miso.PageRes[Res]{Payload: payload, Page: miso.RespPage(p.reqPage, total)}
 	return res, nil
 }
 
---
+++ miso/page.go
@@ -0,0 +1,47 @@
+package miso
+
+const (
+	DefaultPageLimit = 30
+)
+
+type Paging struct {
+	Limit int `json:"limit" desc:"page limit"`
+	Page  int `json:"page" desc:"page number, 1-based"`
+	Total int `json:"total" desc:"total count"`
+}
+
+type PageRes[T any] struct {
+	Page    Paging `json:"paging" desc:"pagination parameters"`
+	Payload []T    `json:"payload" desc:"payload values in current page"`
+}
+
+func (p Paging) GetPage() int {
+	if p.Page < 1 {
+		return 1
+	}
+	return p.Page
+}
+
+func (p Paging) GetOffset() int {
+	return (p.GetPage() - 1) * p.GetLimit()
+}
+
+func (p Paging) GetLimit() int {
+	if p.Limit < 1 {
+		p.Limit = DefaultPageLimit
+	}
+	return p.Limit
+}
+
+func (p Paging) ToRespPage(total int) Paging {
+	return RespPage(p, total)
+}
+
+/* Build Paging for response */
+func RespPage(reqPage Paging, total int) Paging {
+	return Paging{
+		Limit: reqPage.GetLimit(),
+		Page:  reqPage.GetPage(),
+		Total: total,
+	}
+}
