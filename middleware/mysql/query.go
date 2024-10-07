f1e109933b807ed949690394c8a9064f66ae4dce
yongj.zhuang <yongj.zhuang@outlook.com>
2024-10-07 20:19:22 +0800

    *Query based PageQuery

--- middleware/mysql/query.go
+++ middleware/mysql/query.go
@@ -1,6 +1,8 @@
 package mysql
 
 import (
+	"github.com/curtisnewbie/miso/miso"
+	"github.com/curtisnewbie/miso/util"
 	"gorm.io/gorm"
 )
 
@@ -266,4 +268,80 @@ func (q *Query) DB() *gorm.DB {
 
 func NewQuery(db *gorm.DB) *Query {
 	return &Query{tx: db, _db: db, updateColumns: map[string]any{}}
+}
+
+type ChainedPageQuery func(q *Query) *Query
+
+// Create param for page query.
+type PageQuery[V any] struct {
+	db          *gorm.DB
+	selectQuery ChainedPageQuery  // Add SELECT query and ORDER BY query, e.g., return tx.Select(`*`).
+	baseQuery   ChainedPageQuery  // Base query, e.g., return tx.Table(`myTable`).Where(...)
+	mapTo       util.Transform[V] // callback triggered on each record, the value returned will overwrite the value passed in.
+}
+
+func NewPagedQuery[V any](db *gorm.DB) *PageQuery[V] {
+	return &PageQuery[V]{}
+}
+
+func (pq *PageQuery[V]) WithSelectQuery(qry ChainedPageQuery) *PageQuery[V] {
+	pq.selectQuery = qry
+	return pq
+}
+
+func (pq *PageQuery[V]) WithBaseQuery(qry ChainedPageQuery) *PageQuery[V] {
+	pq.baseQuery = qry
+	return pq
+}
+
+func (pq *PageQuery[V]) Transform(t util.Transform[V]) *PageQuery[V] {
+	pq.mapTo = t
+	return pq
+}
+
+func (pq *PageQuery[V]) Scan(rail miso.Rail, reqPage miso.Paging) (miso.PageRes[V], error) {
+	newQuery := func() *Query {
+		return pq.baseQuery(NewQuery(pq.db))
+	}
+
+	countFuture := util.RunAsync(func() (int, error) {
+		var total int
+		_, err := newQuery().Select("COUNT(*)").Scan(&total)
+		return total, err
+	})
+
+	pageFuture := util.RunAsync(func() ([]V, error) {
+		var payload []V
+		qry := newQuery()
+		if pq.selectQuery != nil {
+			qry = pq.selectQuery(qry)
+		}
+		_, err := qry.Offset(reqPage.GetOffset()).
+			Limit(reqPage.GetLimit()).
+			Scan(&payload)
+		if err != nil {
+			return nil, err
+		}
+
+		if pq.mapTo != nil {
+			for i := range payload {
+				payload[i] = pq.mapTo(payload[i])
+			}
+		}
+		return payload, nil
+	})
+
+	var res miso.PageRes[V]
+	total, err := countFuture.Get()
+	if err != nil {
+		return res, err
+	}
+
+	payload, err := pageFuture.Get()
+	if err != nil {
+		return res, err
+	}
+
+	res = miso.PageRes[V]{Payload: payload, Page: miso.RespPage(reqPage, total)}
+	return res, nil
 }
