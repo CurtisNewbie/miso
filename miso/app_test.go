0f58b1e4d39f049fd0217aa70e5b49abeaa279da
yongj.zhuang <yongj.zhuang@outlook.com>
2024-11-05 16:56:05 +0800

    Rename server.go to web.go

--- miso/app_test.go
+++ miso/app_test.go
@@ -180,35 +180,3 @@ func TestSetHeaderTag(t *testing.T) {
 	}
 	t.Logf("after %#v, Desc: %v", d, *d.Desc)
 }
-
-func BenchmarkSetHeaderTag(b *testing.B) {
-	type dummy struct {
-		Name string  `header:"name"`
-		Desc *string `header:"desc"`
-		Age  int     `header:"age"`
-	}
-	GetHeader := func(k string) string {
-		switch k {
-		case "name":
-			return "myname"
-		case "desc":
-			return "this is a test"
-		case "age":
-			return "???"
-		}
-		return ""
-	}
-
-	d := dummy{}
-	var err error
-	callback := walkHeaderTagCallback(GetHeader)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		err = util.WalkTagShallow(&d, callback)
-	}
-
-	if err != nil {
-		b.Fatal(err)
-	}
-}
---
+++ miso/web_test.go
@@ -0,0 +1,39 @@
+package miso
+
+import (
+	"testing"
+
+	"github.com/curtisnewbie/miso/util"
+)
+
+func BenchmarkSetHeaderTag(b *testing.B) {
+	type dummy struct {
+		Name string  `header:"name"`
+		Desc *string `header:"desc"`
+		Age  int     `header:"age"`
+	}
+	GetHeader := func(k string) string {
+		switch k {
+		case "name":
+			return "myname"
+		case "desc":
+			return "this is a test"
+		case "age":
+			return "???"
+		}
+		return ""
+	}
+
+	d := dummy{}
+	var err error
+	callback := walkHeaderTagCallback(GetHeader)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err = util.WalkTagShallow(&d, callback)
+	}
+
+	if err != nil {
+		b.Fatal(err)
+	}
+}
