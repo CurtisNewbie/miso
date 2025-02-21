b35e3aeb577098fcd99ae15205d5afe1a075b5ea
yongj.zhuang <yongj.zhuang@outlook.com>
2025-02-21 17:39:09 +0800

    Get miso version from buildinfo

--- version/version.go
+++ version/version.go
@@ -1,5 +1,23 @@
 package version
 
-const (
+import (
+	"runtime/debug"
+)
+
+var (
 	Version = "v0.1.15-beta.2"
 )
+
+func init() {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if ok {
+		for _, dep := range buildInfo.Deps {
+			if dep.Path == "github.com/curtisnewbie/miso" {
+				if dep.Version != "" {
+					Version = dep.Version
+				}
+				break
+			}
+		}
+	}
+}
