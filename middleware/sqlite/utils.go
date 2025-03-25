012535cfd75b6e8bc61011ecda1e7bb8b2df3fe1
yongj.zhuang <yongj.zhuang@outlook.com>
2025-03-25 11:49:14 +0800

    Add sqlite TableHasColumnFunc(..), TableHasColumn(..)

---
+++ middleware/sqlite/utils.go
@@ -0,0 +1,18 @@
+package sqlite
+
+import "gorm.io/gorm"
+
+func TableHasColumnFunc(table string, column string) func(*gorm.DB) (ok bool, err error) {
+	return func(d *gorm.DB) (ok bool, err error) {
+		return TableHasColumn(table, column, d)
+	}
+}
+
+func TableHasColumn(table string, column string, d *gorm.DB) (ok bool, err error) {
+	var c int
+	err = d.Raw(`SELECT COUNT(*) FROM pragma_table_info(?) WHERE name = ?`, table, column).Scan(&c).Error
+	if err != nil {
+		return false, err
+	}
+	return c < 1, nil
+}
