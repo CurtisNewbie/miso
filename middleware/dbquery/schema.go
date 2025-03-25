bc16d7218addb477b3275f7dcb18b64da7b5fe6f
yongj.zhuang <yongj.zhuang@outlook.com>
2025-03-25 11:18:15 +0800

    Add InitSchemaConditionally(..)

--- middleware/dbquery/schema.go
+++ middleware/dbquery/schema.go
@@ -20,3 +20,28 @@ func InitSchema(rail miso.Rail, initSchemaSegments []string, getDB func() *gorm.
 	rail.Infof("Schema initialized, took: %v", time.Since(start))
 	return nil
 }
+
+type ConditionalSchemaSegment struct {
+	Script    string
+	Condition func(*gorm.DB) (ok bool) // considered true if Condition is nil
+}
+
+func InitSchemaConditionally(rail miso.Rail, conditionalSegments []ConditionalSchemaSegment, getDB func() *gorm.DB) error {
+	rail.Info("Initializing schema")
+	start := time.Now()
+	db := getDB()
+	for _, seg := range conditionalSegments {
+		var ok bool = true
+		if seg.Condition != nil {
+			ok = seg.Condition(db)
+		}
+		if ok {
+			if err := db.Exec(seg.Script).Error; err != nil {
+				return miso.UnknownErrf(err, "Failed to executed '%v'", seg)
+			}
+			rail.Debugf("Executed: '%v'", seg)
+		}
+	}
+	rail.Infof("Schema initialized, took: %v", time.Since(start))
+	return nil
+}
