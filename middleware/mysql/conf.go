a29a9c60cb7254d1af1f499b42b6b0f870d029da
yongj.zhuang <yongj.zhuang@outlook.com>
2025-06-16 13:44:47 +0800

    Fix dsn name for connection collation

--- middleware/mysql/conf.go
+++ middleware/mysql/conf.go
@@ -23,7 +23,7 @@ const (
 	// misoconfig-prop: port | 3306
 	PropMySQLPort = "mysql.port"
 
-	// misoconfig-prop: connection parameters (slices of strings) | `[]string{"charset=utf8mb4", "parseTime=true", "loc=Local", "readTimeout=30s", "writeTimeout=30s", "timeout=3s", "collation_connection=utf8mb4_general_ci"}`
+	// misoconfig-prop: connection parameters (slices of strings) (see [go-sql-driver/mysql](https://github.com/go-sql-driver/mysql?tab=readme-ov-file#dsn-data-source-name)) | `[]string{"charset=utf8mb4", "parseTime=true", "loc=Local", "readTimeout=30s", "writeTimeout=30s", "timeout=3s", "collation=utf8mb4_general_ci"}`
 	PropMySQLConnParam = "mysql.connection.parameters"
 
 	// misoconfig-prop: connection lifetime in minutes (hikari recommends 1800000, so we do the same thing) | 30
@@ -42,7 +42,7 @@ func init() {
 	miso.SetDefProp(PropMySQLUser, "root")
 	miso.SetDefProp(PropMySQLHost, "localhost")
 	miso.SetDefProp(PropMySQLPort, 3306)
-	miso.SetDefProp(PropMySQLConnParam, []string{"charset=utf8mb4", "parseTime=true", "loc=Local", "readTimeout=30s", "writeTimeout=30s", "timeout=3s", "collation_connection=utf8mb4_general_ci"})
+	miso.SetDefProp(PropMySQLConnParam, []string{"charset=utf8mb4", "parseTime=true", "loc=Local", "readTimeout=30s", "writeTimeout=30s", "timeout=3s", "collation=utf8mb4_general_ci"})
 	miso.SetDefProp(PropMySQLConnLifetime, 30)
 	miso.SetDefProp(PropMySQLMaxOpenConns, 10)
 	miso.SetDefProp(PropMySQLMaxIdleConns, 10)
