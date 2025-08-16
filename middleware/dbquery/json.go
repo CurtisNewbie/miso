1f0a72d97488feb66237e48d350ede03b2c7eec7
yongj.zhuang <yongj.zhuang@outlook.com>
2025-08-16 17:25:34 +0800

    Overwrite gorm default json serializer

---
+++ middleware/dbquery/json.go
@@ -0,0 +1,46 @@
+package dbquery
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"github.com/curtisnewbie/miso/encoding/json"
+	"gorm.io/gorm/schema"
+)
+
+func init() {
+	// overwrite default json serializer
+	schema.RegisterSerializer("json", MisoJSONSerializer{})
+}
+
+// MisoJSONSerializer json serializer
+type MisoJSONSerializer struct {
+}
+
+// Scan implements serializer interface
+func (MisoJSONSerializer) Scan(ctx context.Context, field *schema.Field, dst reflect.Value, dbValue interface{}) (err error) {
+	fieldValue := reflect.New(field.FieldType)
+
+	if dbValue != nil {
+		var bytes []byte
+		switch v := dbValue.(type) {
+		case []byte:
+			bytes = v
+		case string:
+			bytes = []byte(v)
+		default:
+			return fmt.Errorf("failed to unmarshal JSONB value: %#v", dbValue)
+		}
+
+		err = json.ParseJson(bytes, fieldValue.Interface())
+	}
+
+	field.ReflectValueOf(ctx, dst).Set(fieldValue.Elem())
+	return
+}
+
+// Value implements serializer interface
+func (MisoJSONSerializer) Value(ctx context.Context, field *schema.Field, dst reflect.Value, fieldValue interface{}) (interface{}, error) {
+	return json.WriteJson(fieldValue)
+}
