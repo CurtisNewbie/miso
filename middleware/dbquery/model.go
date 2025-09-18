c6477dc18d5d0775a2a45dec766db17b333ced10
yongj.zhuang <yongj.zhuang@outlook.com>
2025-09-18 17:31:07 +0800

    CreateModelHook was a bad idea, have to give up :(

--- middleware/dbquery/model.go
+++ middleware/dbquery/model.go
@@ -6,37 +6,33 @@ import (
 )
 
 var (
-	CreatedByExtractor func(rail miso.Rail) string = func(rail miso.Rail) string {
-		return rail.Username()
-	}
+	// createModelType = reflect.TypeOf(CreateModel{})
+	// CreatedByExtractor func(rail miso.Rail) string = func(rail miso.Rail) string {
+	// 	return rail.Username()
+	// }
+
 	UpdatedByExtractor func(rail miso.Rail) string = func(rail miso.Rail) string {
 		return rail.Username()
 	}
 )
 
-// UpdateModel.
-//
-// This is for reference only. The UpdateModel Hooks will still work without this type.
-type UpdateModel struct {
-	UpdatedBy string
-	TraceId   string
-}
-
 // CreateModel.
-//
-// Must be embedded as pointer otherwise the CreateModel hooks won't work.
 type CreateModel struct {
 	CreatedBy string
 	TraceId   string
 }
 
-func (c *CreateModel) SetupCreateModel(cm CreateModel) {
+/*
+
+func (c *CreateModel) SetupCreateModel(cm CreateModel) bool {
 	if c != nil {
 		c.TraceId = cm.TraceId
 		if c.CreatedBy == "" {
 			c.CreatedBy = cm.CreatedBy
 		}
+		return true
 	}
+	return false
 }
 
 // Prepare CreateModel Hook that will run when [Query] call INSERT related methods, see [AddCreateHooks].
@@ -56,21 +52,67 @@ func PrepareCreateModelHook() {
 	AddCreateHooks(func(table string, q *Query, v any) {
 		r, ok := q.Rail()
 		if ok {
-			if cm, ok := v.(interface {
-				SetupCreateModel(CreateModel)
-			}); ok {
-				cm.SetupCreateModel(CreateModel{
+
+			setupCreateModel := func(v any) {
+				if v == nil {
+					return
+				}
+
+				target, ok := v.(interface {
+					SetupCreateModel(CreateModel) bool
+				})
+				if !ok {
+					return
+				}
+
+				cm := CreateModel{
 					TraceId:   r.TraceId(),
 					CreatedBy: r.Username(),
-				})
+				}
+				done := target.SetupCreateModel(cm)
+				if done {
+					return
+				}
+
+				rv := reflect.ValueOf(v)
+				if rv.Kind() != reflect.Ptr {
+					return
+				}
+				for i := 0; i < rv.NumField(); i++ {
+					field := rv.Field(i)
+					typeField := rv.Type().Field(i)
+
+					if typeField.Anonymous && field.Type() == createModelType {
+						field.Set(reflect.New(createModelType))
+						_ = target.SetupCreateModel(cm)
+						return
+					}
+				}
+			}
+
+			rv := reflect.ValueOf(v)
+			switch rv.Kind() {
+			case reflect.Slice, reflect.Array:
+				for i := 0; i < rv.Len(); i++ {
+					ele := rv.Index(i)
+					setupCreateModel(ele.Interface())
+				}
+			default:
+				setupCreateModel(v)
 			}
 		}
 	})
 }
 
+*/
+
 // Prepare UpdateModel Hook that will run when [Query] call UPDATED related methods, see [AddUpdateHooks].
 //
-// Return whether the hook should run for current table, e.g., some table may not have trace_id and updated_by fields.
+// This hook will attempt to extract trace_id and updated_by field from trace ([miso.Rail]) and and update these values to database.
+//
+// Return whether the hook should run for current table, e.g., some tables may not have the trace_id and updated_by fields.
+//
+// By default, hooks are executed for all tables unless optionalFn is provided.
 //
 // Call this func before miso bootstraps.
 func PrepareUpdateModelHooks(optionalFn ...func(table string) (ok bool)) error {
@@ -87,7 +129,7 @@ func PrepareUpdateModelHooks(optionalFn ...func(table string) (ok bool)) error {
 
 				if q.updateColumns != nil {
 					if _, ok := q.updateColumns["updated_by"]; !ok {
-						q.Set("updated_by", r.Username())
+						q.Set("updated_by", UpdatedByExtractor(r))
 					}
 				}
 			}
