00f4d8291929a3005649d79ec6d69f2f0027d580
yongj.zhuang <yongj.zhuang@outlook.com>
2025-09-24 14:34:53 +0800

    Impl Rail.NewTrace()

--- miso/ctx.go
+++ miso/ctx.go
@@ -295,16 +295,13 @@ func (r Rail) WithCtxVal(key string, val any) Rail {
 	return NewRail(ctx)
 }
 
+func (r Rail) NewTrace() Rail {
+	return r.NewCtx().WithCtxVal(XTraceId, NewTraceId()).WithCtxVal(XSpanId, NewSpanId())
+}
+
 // Create a new Rail with a new SpanId and a new Context
 func (r Rail) NextSpan() Rail {
-	prev := r.ctx
-	r.ctx = context.Background() // avoid using the cancelled context in a new goroutine
-
-	// copy values from previous context
-	for _, k := range GetPropagationKeys() {
-		r = r.WithCtxVal(k, prev.Value(k))
-	}
-	return r.WithCtxVal(XSpanId, NewSpanId())
+	return r.NewCtx().WithCtxVal(XSpanId, NewSpanId())
 }
 
 // Create a new Rail with a new Context
