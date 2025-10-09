0256559445c7d0ba9213957156d6f51a328daf05
yongj.zhuang <yongj.zhuang@outlook.com>
2025-10-09 18:18:37 +0800

    Polishing

--- util/pool_test.go
+++ util/pool_test.go
@@ -15,3 +15,35 @@ func TestFixedPool(t *testing.T) {
 		t.Logf("%v", v)
 	}
 }
+
+func TestEphPool(t *testing.T) {
+	p := NewEphPool[int](func(t int) (dropped bool) { return false })
+	for i := 0; i < 10; i++ {
+		p.Push(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		v, ok := p.Pop()
+		if !ok {
+			t.Fatalf("not okay, i: %v", i)
+		}
+		t.Logf("%v", v)
+	}
+
+	_, ok := p.Pop()
+	if ok {
+		t.Fatalf("okay")
+	}
+
+	for i := 0; i < 10; i++ {
+		p.Push(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		v, ok := p.Pop()
+		if !ok {
+			t.Fatalf("not okay, i: %v", i)
+		}
+		t.Logf("%v", v)
+	}
+}
