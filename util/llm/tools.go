f2ce1b45cded63ee580e5a6ddc3a5a4147f8d0b7
yongj.zhuang <yongj.zhuang@outlook.com>
2025-10-17 14:24:42 +0800

    Polishing

--- util/llm/tools.go
+++ util/llm/tools.go
@@ -20,6 +20,15 @@ func parseTag(finalPat *regexp.Regexp, halfPat *regexp.Regexp, s string) string 
 }
 
 // Extract content in <tag>...</tag>.
+func MustTagExtractor(tag string) func(answer string) (original string, extracted string) {
+	t, err := TagExtractor(tag)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+// Extract content in <tag>...</tag>.
 func TagExtractor(tag string) (func(answer string) (original string, extracted string), error) {
 	finalPat, err := regexp.Compile(`(?s)<` + tag + `>(.*)<\/` + tag + `>`)
 	if err != nil {
