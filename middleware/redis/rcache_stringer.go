1823a3a05a2a75d7348e563253fcf25a4454e6f2
yongj.zhuang <yongj.zhuang@outlook.com>
2025-11-25 13:11:42 +0800

    Export RCacheV2

--- middleware/redis/rcache_stringer.go
+++ middleware/redis/rcache_stringer.go
@@ -13,59 +13,59 @@ import (
 // and each key is stored independently.
 //
 //	Use [NewRCacheV2] to instantiate.
-type rcacheV2[K any, T any] struct {
+type RCacheV2[K any, T any] struct {
 	c     *RCache[T]
 	toKey func(k K) string
 }
 
-func (r *rcacheV2[K, T]) key(k K) string {
+func (r *RCacheV2[K, T]) key(k K) string {
 	return r.toKey(k)
 }
 
-func (r *rcacheV2[K, T]) Put(rail miso.Rail, k K, t T) error {
+func (r *RCacheV2[K, T]) Put(rail miso.Rail, k K, t T) error {
 	return r.c.Put(rail, r.key(k), t)
 }
 
-func (r *rcacheV2[K, T]) RefreshTTL(rail miso.Rail, k K) error {
+func (r *RCacheV2[K, T]) RefreshTTL(rail miso.Rail, k K) error {
 	return r.c.RefreshTTL(rail, r.key(k))
 }
 
-func (r *rcacheV2[K, T]) Del(rail miso.Rail, k K) error {
+func (r *RCacheV2[K, T]) Del(rail miso.Rail, k K) error {
 	return r.c.Del(rail, r.key(k))
 }
 
-func (r *rcacheV2[K, T]) GetVal(rail miso.Rail, k K) (T, error) {
+func (r *RCacheV2[K, T]) GetVal(rail miso.Rail, k K) (T, error) {
 	return r.GetValElse(rail, k, nil)
 }
 
-func (r *rcacheV2[K, T]) GetValElse(rail miso.Rail, k K, supplier func() (T, error)) (T, error) {
+func (r *RCacheV2[K, T]) GetValElse(rail miso.Rail, k K, supplier func() (T, error)) (T, error) {
 	return r.c.GetValElse(rail, r.key(k), supplier)
 }
 
-func (r *rcacheV2[K, T]) Get(rail miso.Rail, k K) (T, bool, error) {
+func (r *RCacheV2[K, T]) Get(rail miso.Rail, k K) (T, bool, error) {
 	return r.c.Get(rail, r.key(k))
 }
 
-func (r *rcacheV2[K, T]) GetElse(rail miso.Rail, k K, supplier func() (T, bool, error)) (T, bool, error) {
+func (r *RCacheV2[K, T]) GetElse(rail miso.Rail, k K, supplier func() (T, bool, error)) (T, bool, error) {
 	return r.c.GetElse(rail, r.key(k), supplier)
 }
 
-func (r *rcacheV2[K, T]) Exists(rail miso.Rail, k K) (bool, error) {
+func (r *RCacheV2[K, T]) Exists(rail miso.Rail, k K) (bool, error) {
 	return r.c.Exists(rail, r.key(k))
 }
 
-func (r *rcacheV2[K, T]) DelAll(rail miso.Rail) error {
+func (r *RCacheV2[K, T]) DelAll(rail miso.Rail) error {
 	return r.c.DelAll(rail)
 }
 
-func (r *rcacheV2[K, T]) ScanAll(rail miso.Rail, f func(keys []string) error) error {
+func (r *RCacheV2[K, T]) ScanAll(rail miso.Rail, f func(keys []string) error) error {
 	return r.c.ScanAll(rail, f)
 }
 
 // Create new RCache.
 //
 // K type must either be string or implements [fmt.Stringer], if not, it panics.
-func NewRCacheV2[K any, T any](name string, conf RCacheConfig) *rcacheV2[K, T] {
+func NewRCacheV2[K any, T any](name string, conf RCacheConfig) *RCacheV2[K, T] {
 	var k K
 	var toKey func(K) string = nil
 
@@ -88,7 +88,7 @@ func NewRCacheV2[K any, T any](name string, conf RCacheConfig) *rcacheV2[K, T] {
 	}
 
 	c := NewRCache[T](name, conf)
-	return &rcacheV2[K, T]{
+	return &RCacheV2[K, T]{
 		c:     &c,
 		toKey: toKey,
 	}
