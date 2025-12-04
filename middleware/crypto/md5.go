0288fdf65c5bf06b861dcfb3f4deaeeb8a2935ef
yongj.zhuang <yongj.zhuang@outlook.com>
2025-12-04 10:33:29 +0800

    Polishing

--- middleware/crypto/md5.go
+++ middleware/crypto/md5.go
@@ -3,10 +3,16 @@ package crypto
 import (
 	"crypto/md5"
 	"encoding/hex"
+
+	"github.com/curtisnewbie/miso/util/strutil"
 )
 
 func MD5Hex(b []byte) string {
 	md5 := md5.New()
 	md5.Write(b)
 	return hex.EncodeToString(md5.Sum(nil))
+}
+
+func MD5HexStr(b string) string {
+	return MD5Hex(strutil.UnsafeStr2Byt(b))
 }
