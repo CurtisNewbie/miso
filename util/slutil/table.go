2bfb6d43b30c3222326a011fe57cd0d2b70ac805
yongj.zhuang <yongj.zhuang@outlook.com>
2025-12-08 11:20:33 +0800

    slutil.PadTable()

--- util/slutil/table.go
+++ util/slutil/table.go
@@ -7,3 +7,11 @@ func TableColAt(table [][]string, idx int) []string {
 	}
 	return MapTo(table, func(r []string) string { return r[idx] })
 }
+
+func PadTable(table [][]string, colCnt int) {
+	for i, r := range table {
+		if len(r) < colCnt {
+			table[i] = append(r, make([]string, colCnt-len(r))...)
+		}
+	}
+}
