53ca351280bb704f1c3124849f2166218ae3e012
yongj.zhuang <yongj.zhuang@outlook.com>
2026-01-02 15:27:51 +0800

    MinFloat, MaxFloat

--- util/cmputil/num.go
+++ util/cmputil/num.go
@@ -13,3 +13,17 @@ func MaxInt(a int, b int) int {
 	}
 	return b
 }
+
+func MinFloat(a float64, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func MaxFloat(a float64, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
