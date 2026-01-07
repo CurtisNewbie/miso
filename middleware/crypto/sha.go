bc020acbc672faa25b083bee8e6d1f8a49673faf
yongj.zhuang <yongj.zhuang@outlook.com>
2026-01-07 13:56:34 +0800

    SHA256Hex(). SHA256HexStr()

--- middleware/crypto/sha.go
+++ middleware/crypto/sha.go
@@ -2,6 +2,7 @@ package crypto
 
 import (
 	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 
 	"github.com/curtisnewbie/miso/util/strutil"
@@ -15,4 +16,14 @@ func SHA1Hex(b []byte) string {
 
 func SHA1HexStr(b string) string {
 	return SHA1Hex(strutil.UnsafeStr2Byt(b))
+}
+
+func SHA256Hex(b []byte) string {
+	c := sha256.New()
+	c.Write(b)
+	return hex.EncodeToString(c.Sum(nil))
+}
+
+func SHA256HexStr(b string) string {
+	return SHA256Hex(strutil.UnsafeStr2Byt(b))
 }
