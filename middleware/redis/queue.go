dc66a4c8986b8580fd476598e87bb03d37189f33
yongj.zhuang <yongj.zhuang@outlook.com>
2026-01-07 10:36:15 +0800

    RQueue.PopAll

--- middleware/redis/queue.go
+++ middleware/redis/queue.go
@@ -1,6 +1,10 @@
 package redis
 
-import "github.com/curtisnewbie/miso/miso"
+import (
+	"iter"
+
+	"github.com/curtisnewbie/miso/miso"
+)
 
 type rqueue[T any] struct {
 	key string
@@ -12,6 +16,26 @@ func (r rqueue[T]) Push(rail miso.Rail, t T) error {
 
 func (r rqueue[T]) Pop(rail miso.Rail) (T, bool, error) {
 	return RPopJson[T](rail, r.key)
+}
+
+func (r rqueue[T]) PopAll(rail miso.Rail) iter.Seq2[T, error] {
+	return func(yield func(T, error) bool) {
+		for {
+			c, ok, err := r.Pop(rail)
+			if err != nil {
+				if !yield(c, err) {
+					return
+				}
+				continue
+			}
+			if !ok { // empty
+				return
+			}
+			if !yield(c, nil) {
+				return
+			}
+		}
+	}
 }
 
 func NewRQueue[T any](key string) rqueue[T] {
