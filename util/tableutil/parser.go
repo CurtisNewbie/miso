2379d409a518b7579070e8430e062ca3f69b933c
yongj.zhuang <yongj.zhuang@outlook.com>
2026-01-21 14:43:06 +0800

    Impl TableTransform

---
+++ util/tableutil/parser.go
@@ -0,0 +1,157 @@
+package tableutil
+
+import (
+	"fmt"
+	"io"
+	"slices"
+	"strings"
+
+	"github.com/curtisnewbie/miso/middleware/expr"
+	"github.com/curtisnewbie/miso/miso"
+	"github.com/curtisnewbie/miso/util/slutil"
+	"github.com/spf13/cast"
+)
+
+var (
+	rowRangeExprPool = expr.NewPooledExpr[struct{}](100)
+)
+
+// Table Transform
+//
+// Transform rows and columns while reading.
+type TableTransform struct {
+	Name               string // Parser name
+	SkipRowRangeExpr   string // Skip row range expression. [0] - start row index, [1] - end row index. E.g., `[0, 3]` or `[]`.
+	InclColRangeExpr   string // Include column range expression. [0] - start col index, [1] - end col index. E.g., `[0, 3]` or `[]`.
+	HeaderRowRangeExpr string // Header row range expression. [0] - start row index, [1] - end row index. E.g., `[0, 3]` or `[]`
+	RowSeperator       string // Row Seperator, default to `'\n'`
+	ColSeperator       string // Col Seperator, default to `'  '` (two spaces)
+}
+
+func (p TableTransform) HeaderRowRange() (Range, error) {
+	return p.parseRowRange(p.HeaderRowRangeExpr)
+}
+
+func (p TableTransform) SkipRowRange() (Range, error) {
+	return p.parseRowRange(p.SkipRowRangeExpr)
+}
+
+func (p TableTransform) InclColRange() (Range, error) {
+	return p.parseRowRange(p.InclColRangeExpr)
+}
+
+func (p TableTransform) parseRowRange(s string) (Range, error) {
+	if s == "" {
+		return Range{Min: -1, Max: -1}, nil
+	}
+	v, err := rowRangeExprPool.Eval(s, struct{}{})
+	if err != nil {
+		return Range{}, err
+	}
+	is := cast.ToIntSlice(v)
+	min := -1
+	max := -1
+	slices.Sort(is)
+	if len(is) > 0 {
+		min = is[0]
+		max = is[len(is)-1]
+	}
+	return Range{Min: min, Max: max}, nil
+}
+
+func (p TableTransform) WriteRow(buf *strings.Builder, header []string, inclColRange Range, r []string) {
+	if buf.Len() > 0 {
+		buf.WriteRune('\n')
+		buf.WriteString(p.RowSeperator)
+	}
+
+	sp := p.ColSeperator
+	if sp == "" {
+		sp = strings.Repeat(" ", 2)
+	}
+
+	fst := true
+	for i, v := range r {
+		if !inclColRange.Nil() && !inclColRange.In(i) {
+			continue
+		}
+		v = strings.TrimSpace(v)
+		if v == "" {
+			continue
+		}
+		if !fst {
+			_, _ = buf.WriteString(sp)
+		}
+		if i < len(header) && header[i] != "" {
+			_, _ = buf.WriteString(fmt.Sprintf("%v: ", header[i]))
+		}
+		_, _ = buf.WriteString(v)
+		fst = false
+	}
+}
+
+func (p TableTransform) ParseFile(rail miso.Rail, reader interface{ Read() ([]string, error) }) (*strings.Builder, error) {
+	skipRowRange, err := p.SkipRowRange()
+	if err != nil {
+		return nil, err
+	}
+	inclColRange, err := p.InclColRange()
+	if err != nil {
+		return nil, err
+	}
+	headerRowRange, err := p.HeaderRowRange()
+	if err != nil {
+		return nil, err
+	}
+	rail.Infof("SkipRowRange: %#v, InclColRange: %#v, HeaderRowRange: %#v", skipRowRange, inclColRange, headerRowRange)
+
+	var header []string
+	b := &strings.Builder{}
+	validRow := func(i int, row []string) bool {
+		if skipRowRange.In(i) {
+			return false
+		}
+
+		if headerRowRange.In(i) {
+			header = slutil.Copy(row)
+			return false
+		}
+
+		for j, c := range row {
+			if !inclColRange.Nil() && !inclColRange.In(j) {
+				continue
+			}
+			if c != "" {
+				return true
+			}
+		}
+		return false
+	}
+	i := 0
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			return b, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		if validRow(i, record) {
+			p.WriteRow(b, header, inclColRange, record)
+		}
+		i++
+	}
+}
+
+type Range struct {
+	Min int
+	Max int
+}
+
+func (c Range) In(i int) bool {
+	return i >= c.Min && i <= c.Max
+}
+
+func (c Range) Nil() bool {
+	return c.Min < 0 && c.Max < 0
+}
--- util/tableutil/table.go
+++ util/tableutil/table.go
@@ -1,16 +1,15 @@
-package slutil
+package tableutil
+
+import "github.com/curtisnewbie/miso/util/slutil"
 
 // Collect table col at idx.
-//
-// Deprecated: use tableutil.TableColAt instead.
 func TableColAt(table [][]string, idx int) []string {
 	if idx < 0 || len(table) < 1 || len(table[0]) <= idx {
 		return []string{}
 	}
-	return MapTo(table, func(r []string) string { return r[idx] })
+	return slutil.MapTo(table, func(r []string) string { return r[idx] })
 }
 
-// Deprecated: use tableutil.PadTable instead.
 func PadTable(table [][]string, colCnt int) {
 	for i, r := range table {
 		if len(r) < colCnt {
